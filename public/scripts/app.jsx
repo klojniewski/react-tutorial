f6f6f9582dba940cd2f1f055c8650872ff1bc5d7
Krzysztof Lojniewski <kontakt@pagepro.pl>
2016-04-17 11:55:43 +0200

    Component Properties

--- public/scripts/app.jsx
+++ public/scripts/app.jsx
@@ -18,10 +18,37 @@ var CommentForm = React.createClass({
   }
 });
 
+var CommentList = React.createClass({
+  render: function () {
+    return (
+      <div className="commentList">
+        <Comment author="Chris Eight" date="24.05.1986">This is one comment</Comment>
+        <Comment author="Jordan Michael" date="24.05.1976">This is *another* comment</Comment>
+      </div>
+    );
+  }
+});
+
+var Comment = React.createClass({
+  render: function () {
+    return (
+      <div className="comment">
+        <h2 className="commentAuthor">
+          {this.props.author}
+        </h2>
+        <span className="commentDate">{this.props.date}</span>
+        <div>
+          {this.props.children}
+        </div>
+      </div>
+    );
+  }
+});
+
 ReactDOM.render(
   <div className="commentBox">
     <h1>Comments</h1>
-    <CommentBox />
+    <CommentList />
     <CommentForm />
   </div>,
   document.getElementById('content')
